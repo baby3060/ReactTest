1e9355c487411eb943d6ebe1a91a45ab76f95422
psk <psk3060@naver.com>
2017-11-24 16:42:19 +0900

    First commit React Test

---
+++ App.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+
+var data2;
+
+function SetData2() {
+    data2 = [
+                {
+                    "id" : 1,
+                    "name":"Foo",
+                    "age":"20"
+                },
+                {
+                    "id" : 2,
+                    "name":"Bar",
+                    "age":"30"
+                },
+                {
+                    "id":3,
+                    "name":"Baz",
+                    "age":"40"
+                }
+            ];
+}
+
+class App extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        var myStyle = {
+            fontSize : '15',
+            color : '#FF0000'
+        }
+        return (
+            <div>
+                <h1 style={myStyle}>Header</h1>
+                <h2>Content</h2>
+                <p date-myattribute="somevalue">This is the Content!!!</p>
+                <JSCal />
+                <p></p>
+                <TBTest />
+            </div>
+        );
+    }
+}
+
+class JSCal extends React.Component {
+    render() {
+        return (
+            <p>{ 1 + 1 }</p>
+        );
+    }
+}
+
+
+
+class TBTest extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            data : data2
+        }
+    }
+    render() {
+        var thStyle = {
+            fontSize : '12',
+            backgroundColor : 'yellow',
+            color : 'red',
+            width : '140px'
+        }
+        return(
+            <table border="1">
+               <thead>
+                    <tr>
+                        <th style={thStyle}>ID</th>
+                        <th style={thStyle}>Name</th>
+                        <th style={thStyle}>Age</th>
+                    </tr>
+               </thead>
+               
+               <tbody>
+                  {this.state.data.map((person, i) => <TableRow key = {i} 
+                     data = {person} />)}
+               </tbody>
+               
+            </table>
+        );
+    }
+}
+
+class TableRow extends React.Component {
+    render() {
+        return (
+         <tr>
+            <td>{this.props.data.id}</td>
+            <td>{this.props.data.name}</td>
+            <td>{this.props.data.age}</td>
+         </tr>
+      );
+    }
+}
+
+SetData2();
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var config = {
+    entry : './app.js',
+    output : {
+        path : '/',
+        filename : 'index.js'
+    },
+    devServer : {
+        inline : true,
+        port : 8181
+    },
+    module : {
+        rules : [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options : {
+                    presets: ['env', 'es2015', 'react', 'stage-0']
+                }
+            },
+            {
+                test:/\.css$/,
+                use : ExtractTextPlugin.extract ({
+                    fallback: 'style-loader',
+                    use : 'css-loader'
+                })
+            },
+            {
+                test : /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader : 'url-loader',
+                options : {
+                    name: '[hash].[ext]',
+                    limit : 10000
+                }
+            }
+        ]
+    },
+    resolve : {
+        modules : ['node_modules'],
+        extensions : ['.js', '.json', '.jsx', '.css']
+    },
+    plugins : [
+        new webpack.LoaderOptionsPlugin({
+            minimize : true,
+            debug : true
+        }),
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new ExtractTextPlugin({
+            filename : 'app.css'
+        })
+    ],
+    
+    
+}
+
+module.exports = config;
