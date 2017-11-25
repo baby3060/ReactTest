e64f0bbd9c9bca4f29a8e0f45c5f59db6e3924b5
psk <psk3060@naver.com>
2017-11-25 18:00:40 +0900

    모듈 분할

---
+++ src/components/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import TBTest from './TBTest';
+import PropEx from './PropEx';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            header : "Header Text Showme",
+            Content : "It is Content Text"
+        }
+        
+    }
+    render() {
+        var myStyle = {
+            fontSize : '15',
+            color : '#FF0000'
+        }
+        return (
+            <div>
+                <h1 style={myStyle}>{this.state.header}</h1>
+                <h2>{this.state.Content}</h2>
+                <p date-myattribute="somevalue">This is the Content!!!</p>
+                <JSCal first="120" />
+                <TBTest IdThText="Id" NaThText="NAME" />
+                <h4>{this.props.h4Text}</h4>
+                <PropEx />
+            </div>
+        );
+    }
+}
+
+App.propTypes = {
+    h4Text: PropTypes.string
+};
+
+App.defaultProps = {
+    h4Text : "이것은 Default 입니다."
+}
+
+class JSCal extends React.Component {
+    render() {
+        return (
+            <p>{parseInt(this.props.first, 10) + 1}</p>
+        );
+    }
+}
+
+export default App;
