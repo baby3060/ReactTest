aec2d8c678171208c9140e593738629605c8249e
psk <psk3060@naver.com>
2017-11-28 00:06:05 +0900

    Update TR Complete & Next Cpu rendering Minium

--- src/components/TBManager.js
+++ src/components/TBManager.js
@@ -23,23 +23,14 @@ class TBManager extends React.Component {
                     "age": 40
                 }
             ],
-            selectedKey : -1
+            selectedKey : -1,
+            selectedUser : [
+                "id" : "",
+                "name" : "",
+                "age" : 0
+            ]
         }
     }
-    
-    P_selectUser(key) {
-        if(key == this.state.selectedKey) {
-           this.setState({
-                selectedKey : -1   
-           });
-        return;
-        }
-        
-        this.setState({
-            selectedKey : key
-        });
-    }
-    
     
     p_insertUser(id, name, age) {
         
@@ -79,6 +70,68 @@ class TBManager extends React.Component {
         }
     }
     
+    userRemove(){
+        if(this.state.selectedKey == -1) {
+            alert("User를 선택바랍니다.");
+            return;  
+        } 
+        
+        this.setState({
+              UserData : stUpdate (
+                 this.state.UserData
+               , {  $splice : [[this.state.selectedKey, 1]] }
+              )
+            , selectedKey : -1
+        });
+        
+    }
+    
+    UserSelected(key) {
+        if( this.state.selectedKey == key ) {
+            // 다시 누를 경우 취소
+            this.setState({
+                selectedUser : stUpdate (
+                    this.state.selectedUser
+                  , {
+                       id : {$set : ""},
+                       name : {$set : ""},
+                       age  : {$set : "0"}
+                    }
+                ), selectedKey : -1
+            });
+            
+            return;
+        } else {
+            this.setState({
+                 selectedUser : this.state.UserData[key]
+                , selectedKey : key
+            });
+        }
+    }
+    
+    p_ModifyUser(id, name, age) {
+        this.setState({
+            UserData : stUpdate(
+                this.state.UserData,
+                {
+                    [this.state.selectedKey] : {
+                        id : {$set : id},
+                        name : {$set : name},
+                        age : {$set : age}
+                    }
+                }
+            ),
+            selectedUser : stUpdate(
+                this.state.selectedUser,
+                {
+                    id : {$set : id},
+                    name : {$set : name},
+                    age : {$set : age}
+                }
+            )
+        });
+    }
+    
     render() {
         let thStyle = {
             fontSize : '12',
@@ -107,13 +160,22 @@ class TBManager extends React.Component {
                                                       <TableRow key = {i} 
                                                                 data = {person} 
                                                                 isSelected={this.f_isSelected_U.bind(this)(i)}
-                                                                onSelUser={this.P_selectUser.bind(this)}
+                                                                onSelUser={this.UserSelected.bind(this)}
                                                                 userKey={i}
                                                         />)}
                         </tbody>
 
                     </table>
                 </div>
+                <RowRemover userRemove={this.userRemove.bind(this)}
+                            selectedKey={this.state.selectedKey}
+                            />
+
+                <RowSelector isSelected={(this.state.selectedKey != -1)}
+                            onSelect={this.UserSelected.bind(this)}
+                            selUser={this.state.selectedUser}
+                            onModify={this.p_ModifyUser.bind(this)}
+                             />
             </div>
         );
     }
@@ -229,12 +291,125 @@ class RowCreator extends React.Component {
     }
 }
 
-
 RowCreator.propTypes = {
     id: PropTypes.string,
     name : PropTypes.string,
     age : PropTypes.number
 };
 
+class RowRemover extends React.Component {
+    remove_click() {
+        this.props.userRemove();
+    }
+    
+    render() {
+        let visiStyle = keyIndex => {
+            let style = {};
+            
+            if( keyIndex == -1 ) {
+                style = { visibility : 'hidden' }
+            } else {
+                style = { visibility : 'visible' }
+            }
+            return style;
+        };
+        
+        return(
+            <button style={ visiStyle(this.props.selectedKey) }
+                    onClick={this.remove_click.bind(this)}>
+                User Delete
+            </button>
+        );
+    }
+}
+
+class RowSelector extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id : "",
+            name : "",
+            age : 0
+        }
+    }
+    
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            id : nextProps.selUser.id,
+            name : nextProps.selUser.name,
+            age : nextProps.selUser.age
+        });
+    }
+    
+    modifyEvent() {
+        let idVal = (this.state.id != "");
+        let nameVal = (this.state.name != "");
+        let ageVal = false;
+        
+        let ageStr = (this.state.age == '')?"0":this.state.age + "";
+        ageVal =(parseInt(ageStr) > 0);
+        
+        let isIn = idVal && nameVal && ageVal;
+        
+        if(isIn) {
+            this.props.onModify(this.state.id, this.state.name, this.state.age);
+            this.setState({
+                id : "",
+                name : "",
+                age : 0
+            });
+        } else {
+            if( !idVal ) {
+               alert("ID 를 입력바랍니다.");
+            } else if(!nameVal) {
+                alert("Name을 입력바랍니다.");
+            } else {
+                alert("age를 입력바랍니다.");
+            }
+            return;
+        }
+    }
+    
+    setObjValue(e) {
+        let nextState = {};
+        nextState[e.target.name] = e.target.value;
+        this.setState(nextState);
+    }
+    
+    render() {
+        let divStyle = isSelected => {
+            let style = {};
+            
+            if( isSelected ) {
+               style = { visibility : 'visible' }
+            } else {
+               style = { visibility : 'hidden' }
+            }
+            return style;
+        };
+        
+        let setVal = this.setObjValue.bind(this);
+        
+        return (
+            <div style={divStyle(this.props.isSelected)}>
+                <input type="text" name="id" placeholder="Id"
+                    value={this.state.id}
+                    onChange={setVal}
+                    />
+                <input type="text" name="name" placeholder="Name" 
+                    value={this.state.name}
+                    onChange={setVal}
+                    />
+                <input type="text" name="age" placeholder="Age" 
+                    value={this.state.age}
+                    onChange={setVal}
+                    />
+                <button onClick={this.modifyEvent.bind(this)}>
+                    Modify User
+                </button>
+            </div>     
+        );
+    }
+}
 
 export default TBManager;
